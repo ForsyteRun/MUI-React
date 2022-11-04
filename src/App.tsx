34ef7b2d68f4a1d9108a1fed51d0bb26baff9b5e
ForsyteRun <forsyterun@gmail.com>
2022-11-04 16:35:42 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Box } from "@mui/material";
+import { MuiTypograthy } from "./components/MuiTypograthy";
 
-function App() {
+export const App: FC = React.memo(() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <MuiTypograthy/>
     </div>
-  );
-}
-
-export default App;
+  // <Box
+  // sx={{
+  //   height: '100vh',
+  //   width: {
+  //     xs: 320, 
+  //     sm: 600,
+  //     md: 900,
+  //     lg: 1200,
+  //     xl: 1536
+  //   },
+  //   bgcolor: 'secondary.main',
+  //   margin: '0 auto'
+  // }}
+  // ></Box>
+  )
+});
